00ba3d3e1571bf6098cb646ae25739226dd31a8a
aaryan <aaryanbajaj385@gmail.com>
2024-11-21 18:56:11 +0530

    feat: nodejs socket server setup socket.io

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,28 @@
+import { Server } from "socket.io";
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init socket server");
+    this._io = new Server();
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("Initialise socket listeners");
+    io.on("connect", (socket) => {
+      console.log("New socket connected", socket.id);
+
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New message recieved ", message);
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
